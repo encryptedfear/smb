90e7265f50d3d9df48a7773d00bcd105c59b62bd
encryptedfear <seph.gallo@gmail.com>
2020-09-22 07:14:48 +0800

    Commitment.

--- server/plugin/plg_backend_smb/index.go
+++ server/plugin/plg_backend_smb/index.go
@@ -14,29 +14,65 @@ var SmbCache AppCache
 
 func init() {
 	Backend.Register("smb", Smb{})
+	SmbCache = NewAppCache()
+
+	SmbCache.OnEvict(func(key string, value interface{}) {
+		s := value.(*Smb)
+		s.Close()
+	})
 }
 
 type Smb struct {
-	params *SmbParams
-}
-
-type SmbParams struct {
-	server   string
-	shared   string
-	username string
-	password string
+	SmbClient *smb2.Share
 }
 
 func (smb Smb) Init(params map[string]string, app *App) (IBackend, error) {
-	backend := Smb{
-		params: &SmbParams{
-			params["server"],
-			params["shared"],
-			params["username"],
-			params["password"],
+	p := struct {
+		server   string
+		shared   string
+		username string
+		password string
+	}{
+		params["server"],
+		params["shared"],
+		params["username"],
+		params["password"],
+	}
+
+	if c := SmbCache.Get(params); c != nil {
+		d := c.(*Smb)
+		return d, nil
+	}
+
+	conn, err := net.Dial("tcp", p.server+":445")
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	d := &smb2.Dialer{
+		Initiator: &smb2.NTLMInitiator{
+			User:     p.username,
+			Password: p.password,
 		},
 	}
-	return backend, nil
+
+	client, err := d.Dial(conn)
+	if err != nil {
+		// client.Logoff()
+		return nil, err
+	}
+
+	fs, err := client.Mount(p.shared)
+	if err != nil {
+		// fs.Umount()
+		return nil, err
+	}
+
+	smb.SmbClient = fs
+	SmbCache.Set(params, &smb)
+	return &smb, nil
+
 }
 
 func (smb Smb) LoginForm() Form {
@@ -73,37 +109,7 @@ func (smb Smb) LoginForm() Form {
 
 func (smb Smb) Ls(path string) ([]os.FileInfo, error) {
 
-	conn, err := net.Dial("tcp", smb.params.server+":445")
-	if err != nil {
-		return nil, err
-	}
-	defer conn.Close()
-
-	d := &smb2.Dialer{
-		Initiator: &smb2.NTLMInitiator{
-			User:     smb.params.username,
-			Password: smb.params.password,
-		},
-	}
-
-	session, err := d.Dial(conn)
-	if err != nil {
-		return nil, err
-	}
-	defer session.Logoff()
-
-	fs, err := session.Mount(smb.params.shared)
-	if err != nil {
-		return nil, err
-	}
-	defer fs.Umount()
-
-	osPath := path
-	if osPath == "/" {
-		osPath = ""
-	}
-
-	fis, err := fs.ReadDir(encodeUNC(osPath))
+	fis, err := smb.SmbClient.ReadDir(encodeUNC(path))
 	if err != nil {
 		return nil, err
 	}
@@ -112,67 +118,56 @@ func (smb Smb) Ls(path string) ([]os.FileInfo, error) {
 }
 
 func (smb Smb) Cat(path string) (io.ReadCloser, error) {
-	return nil, nil
+	remoteFile, err := smb.SmbClient.Open(encodeUNC(path))
+	if err != nil {
+		return nil, err
+	}
+
+	return remoteFile, nil
 }
 
 func (smb Smb) Mkdir(path string) error {
-	return nil
+	err := smb.SmbClient.Mkdir(encodeUNC(path), os.FileMode(0777))
+	return err
 }
 
 func (smb Smb) Rm(path string) error {
-
-	conn, err := net.Dial("tcp", smb.params.server+":445")
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	d := &smb2.Dialer{
-		Initiator: &smb2.NTLMInitiator{
-			User:     smb.params.username,
-			Password: smb.params.password,
-		},
-	}
-
-	session, err := d.Dial(conn)
-	if err != nil {
-		return err
-	}
-	defer session.Logoff()
-
-	fs, err := session.Mount(smb.params.shared)
-	if err != nil {
-		return err
-	}
-	defer fs.Umount()
-
-	osPath := path
-	if osPath == "/" {
-		osPath = ""
-	}
-
-	err = fs.RemoveAll(encodeUNC(osPath))
+	err := smb.SmbClient.RemoveAll(encodeUNC(path))
 	return err
 }
 
 func (smb Smb) Mv(from string, to string) error {
-	return nil
+	err := smb.SmbClient.Rename(encodeUNC(from), encodeUNC(to))
+	return err
 }
 
 func (smb Smb) Touch(path string) error {
-	return nil
+	file, err := smb.SmbClient.Create(encodeUNC(path))
+	if err != nil {
+		return err
+	}
+	_, err = file.ReadFrom(strings.NewReader(""))
+	return err
 }
 
 func (smb Smb) Save(path string, file io.Reader) error {
-	return nil
+	remoteFile, err := smb.SmbClient.Create(encodeUNC(path))
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(remoteFile, file)
+	remoteFile.Close()
+	return err
 }
 
 func (smb Smb) Stat(path string) (os.FileInfo, error) {
-	return nil, nil
+	f, err := smb.SmbClient.Stat(encodeUNC(path))
+	return f, err
 }
 
 func (smb Smb) Close() error {
-	return nil
+	err0 := smb.SmbClient.Umount()
+	return err0
 }
 
 func encodeUNC(path string) string {
